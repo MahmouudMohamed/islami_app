0100ce3ad0c506a31dbd9b30ca83afb0245b47c5
Mahmoud Ouf <mahmoudouf504@gmail.com>
2024-07-09 17:01:14 +0300

    change title

--- lib/home.dart
+++ lib/home.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Iami"),
+        title: Text("Islami"),
       ),
     );
   }
